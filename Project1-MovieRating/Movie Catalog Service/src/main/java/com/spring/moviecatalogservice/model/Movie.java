2053ea74916719ca29f74effb2f3a4cc60056237
aturk <aturk@netas.com.tr>
2020-04-30 21:52:31 +0300

    added eureka client and load balancer annotation

--- Project1-MovieRating/Movie Catalog Service/src/main/java/com/spring/moviecatalogservice/model/Movie.java
+++ Project1-MovieRating/Movie Catalog Service/src/main/java/com/spring/moviecatalogservice/model/Movie.java
@@ -4,6 +4,9 @@ public class Movie {
     private String movieId;
     private String name;
 
+    public Movie() {
+    }
+
     public Movie(String movieId, String name) {
         this.movieId = movieId;
         this.name = name;
